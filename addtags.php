14ce15f3f098ff5e5d81fbfdff6cec44f2979769
mschnaars <mschnaar@umail.iu.edu>
2017-06-14 13:08:18 -0400

    cleaned up directory

--- addtags.php
+++ addtags.php
@@ -1,3 +1,12 @@
 <?php
 
+	Get objectID and tag input via POST method from input box, assign to variables
+		tag string = exploded array of all tags;
+
+	SQL query to retrieve all tags from database
+		if (tags != existing tags)
+			new sql statement to insert new tags to tags table;
+
+	new sql statements to attach tags to images in the objecttags table;
+
 ?>
